0012541d3b515999bd55f21353bcb8c174021ed9
Corey Rowe <coreyr1196@gmail.com>
2018-12-17 11:22:19 -0500

    Add command to trigger snap

--- bot.js
+++ bot.js
@@ -12,9 +12,22 @@ client.on("message", async message => {
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
+	
+	const snapTrigger = `snap ${message.channel.name}`;
 
-	if(command === "test") {
-		message.channel.send("Fine. I'll do it myself.");
+	if (command == 'snap') {
+		if (args == message.channel.name) {
+			message.channel.send("*snap*");
+		}else{
+			message.channel.send(`**You didn't type the channel name correctly.**`);
+			message.channel.send(`I need to be sure you know what you're about to do.`);
+			message.channel.send(`Type \`${config.prefix} ${snapTrigger}\` to snap this channel.`);
+			message.channel.send(`**This will DELETE all unpinned messages in this channel. You have been warned.**`);
+		}
+	}else{
+		message.channel.send("Fun isn't something one considers when balancing a Discord server.");
+		message.channel.send(`Type \`${config.prefix} ${snapTrigger}\`. When I'm done, all pinned messages on this channel will still exist.`);
+		message.channel.send(`**This will DELETE all unpinned messages in this channel. You have been warned.**`);
 	}
 });
 
